5f6ad544e966fa6397131fb74de9bd8c9666c43a
qianlei <qianlei@sunlands.com>
2019-11-12 14:42:05 +0800

    add sulation exam 206

--- src/main/java/win/suroot/leetCode/Examination1.java
+++ src/main/java/win/suroot/leetCode/Examination1.java
@@ -12,13 +12,13 @@ public class Examination1 {
 
     /**
      * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
-     *
+     * <p>
      * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
-     *
+     * <p>
      * 示例:
-     *
+     * <p>
      * 给定 nums = [2, 7, 11, 15], target = 9
-     *
+     * <p>
      * 因为 nums[0] + nums[1] = 2 + 7 = 9
      * 所以返回 [0, 1]
      *
@@ -29,11 +29,10 @@ public class Examination1 {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
-                    int[] result = {i, j};
-                    return result;
+                    return new int[]{i, j};
                 }
             }
         }
-        return null;
+        return new int[]{0};
     }
 }
--- src/main/java/win/suroot/leetCode/Examination206.java
+++ src/main/java/win/suroot/leetCode/Examination206.java
@@ -10,26 +10,54 @@ public class Examination206 {
 
     /**
      * 反转一个单链表。
-     * <p>
      * 示例:
-     * <p>
      * 输入: 1->2->3->4->5->NULL
      * 输出: 5->4->3->2->1->NULL
      * 进阶:
      * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
-     *
+     */
+
+
+    /**
      * @author: qianlei
-     * @date: 2019-11-09 23:55
+     * @date: 2019-11-12 11:11
      */
     public static ListNode reverseList(ListNode head) {
-        ListNode next = head.next;
-        if (next.next == null) {
-            next.next = head;
-            return head;
-        } else {
-            reverseList(next);
+        return each(head, null);
+    }
+
+    private static ListNode each(ListNode current, ListNode preview) {
+        ListNode temp = current.next;
+        current.next = preview;
+        preview = current;
+        current = temp;
+
+        if (current != null) {
+            return each(current, preview);
         }
-        return head;
+        return null;
+    }
+
+    /**
+     * @author: qianlei
+     * @date: 2019-11-12 11:11
+     */
+    public static ListNode reverseList1(ListNode head) {
+        // 当前指针前节点
+        ListNode preview = null;
+        // 当前节点
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode temp = current.next;
+            current.next = preview;
+            // 此处 有疑问？ current.next = preview  preview 又等于current
+            // 理解：current.next = preview 赋值是给的是引用，此时current.next和preview 指向的都是null 的地址，当preview = current 时preview 指向的是current的对象地址。
+            preview = current;
+            // current内存地址被preview指向，现在有两个引用指向current对象的地址，current得以解脱，把current指向tem对象所指向的内存。即下一个节点
+            current = temp;
+        }
+        return preview;
     }
 
 
